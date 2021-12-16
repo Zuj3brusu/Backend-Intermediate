3baf8c0fad88ef209b242096cb0bc3ab2b5473d4
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-16 19:03:21 +0530

    Add dotenv using @nestjs/config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('env')
+  getEnvData(): string {
+    return this.appService.getEnvData();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  
+  getEnvData() : string {
+    return process.env.DB_USER
+  }
 }
