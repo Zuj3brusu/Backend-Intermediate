fe664f8c85da440c43af1497c341282084128fca
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-17 16:45:12 +0530

    Change sequelize to use dotenv info

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,14 +5,14 @@ export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '.',
-        database: 'WorkDB',
-        dialect:'postgres',
-      });
+      const sequelize = new Sequelize(
+        process.env.DB_DATABASE,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        dialect: 'postgres',
+        });
       sequelize.addModels([User]);
       await sequelize.sync();
       return sequelize;
