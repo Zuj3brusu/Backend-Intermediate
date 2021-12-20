dadcbd00cd4db8300b2b32f274f5fa05dbd315cf
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-20 18:13:13 +0530

    Add Login authentication using passport-jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+  ) {}
 
   @Get()
   getHello(): string {
@@ -13,5 +19,17 @@ export class AppController {
   @Get('env')
   getEnvData(): string {
     return this.appService.getEnvData();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../modules/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/modules/users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService
+    ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+    if (user) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch){
+            const { password, ...result } = user;
+            return result;
+        }
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.dataValues.email, sub: user.dataValues.firstName };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+}
