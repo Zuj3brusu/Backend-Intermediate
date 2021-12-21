f1fb40d91530f3f414f9cf703bd2da7f2404f13c
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-21 21:53:42 +0530

    create a graphql resolver property on the user that returns all posts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { usersProviders } from './users.providers';
 import { UsersResolver } from './users.resolver';
 import { DatabaseModule } from '../../database/database.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule,PostsModule],
   controllers: [],
   providers: [
     UsersService,
--- src/modules/users/users.resolver.ts
+++ src/modules/users/users.resolver.ts
@@ -1,11 +1,17 @@
 import {User} from './user.entity'
 import {Resolver, Query, ResolveField, Args, Parent, Mutation} from '@nestjs/graphql'
 import { UsersService } from './users.service';
+import { PostsService } from '../posts/posts.service';
+import { CurrentUser } from 'src/auth/auth.resolver';
+import { Post } from '../posts/post.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/graphql-auth.guard';
 
 @Resolver(of => User)
 export class UsersResolver {
   constructor(
     private usersService: UsersService,
+    private postsService: PostsService,
   ) {}
 
   @Query()
@@ -17,6 +23,13 @@ export class UsersResolver {
   async getUserByEmail(obj, args, context, info) {
     const { email } = args;
     return await this.usersService.findOneByEmail(email);
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Query()
+  async getMyPosts(@CurrentUser() user: User) {
+    const email = user.email
+    return await this.postsService.findAllByEmail(email);
   }
 
   @Mutation('createUser')
