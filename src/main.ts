2678ab2b8315f20b93a4e0f783d2728dcad97028
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-27 18:09:57 +0530

    Create a proxy server

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ var proxy = require('express-http-proxy');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/proxy', proxy("www.google.com"));
+  app.use('/proxy', proxy('localhost:3000', {
+    proxyReqPathResolver: function(req) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {   // simulate async
+          var parts = req.url.split('?');
+          var queryString = parts[1];
+          var updatedPath = parts[0].replace(/test/, 'tent');
+          var resolvedPathValue = updatedPath + (queryString ? '?' + queryString : '');
+          resolve(resolvedPathValue);
+        }, 200);
+      });
+    }
+  }));
   // global prefix
   // app.setGlobalPrefix('api/v1');
   await app.listen(3000);
