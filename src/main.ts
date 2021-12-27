ce331768ad52500104832e15f663be5db5994bce
Pulkit Gaur <gaurpulkit99@gmail.com>
2021-12-27 17:41:59 +0530

    add expess-http-proxy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var proxy = require('express-http-proxy');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/proxy', proxy("www.google.com"));
   // global prefix
   // app.setGlobalPrefix('api/v1');
   await app.listen(3000);
